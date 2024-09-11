38babba5e058a3cdd78a847d6e812f6e218a2d52
ronseriajorge <ronseriajorge@gmail.com>
2024-09-11 16:25:24 -0500

    Se agrega la logica para el manejo de las ordenes

--- coffee_shop/urls.py
+++ coffee_shop/urls.py
@@ -2,7 +2,7 @@
 URL configuration for coffee_shop project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,13 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
+    path("", include("products.urls")),
     path("admin/", admin.site.urls),
-    path("productos/",include('products.urls')),
-    path("usuarios/",include('users.urls'))
-] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    path("usuarios/", include("users.urls")),
+    path("pedidos/", include("orders.urls")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
